72252a64b0c7afc7a55d83e1199fc111a0e46186
Nguyen Hanh <ndhanh96@gmail.com>
2017-12-25 18:11:43 +0700

    update color when hover

--- jquery.js
+++ jquery.js
@@ -1,10 +1,15 @@
-window.onload = function what() {
-   // var c = document.getElementById("canvas");
-   // var ctx = c.getContext("2d");
-   // ctx.fillStyle="#FF0000";
-   // ctx.fillRect(150,150,150,75);
-   // ctx.beginPath();
-   // ctx.moveTo(300, 0);
-   // ctx.quadraticCurveTo(80, 80, 0, 300);
-   // ctx.stroke();
-};
+$(document).ready(function(){
+   $("#canvas4").hover(function() {
+      $("#canvas4").css("background", "#0bb8ff");
+   });
+   $("#canvas4").mouseleave(function(){
+      $("#canvas4").css("background", "skyblue");
+   });
+
+   $("#canvas2").hover(function() {
+      $("#canvas2").css("background", "red");
+   });
+   $("#canvas2").mouseleave(function(){
+      $("#canvas2").css("background", "#bf3333");
+   });
+});

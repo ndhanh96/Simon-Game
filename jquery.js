ea66be64c929ec955bc9cf650266a913fb4fcd1e
Nguyen Hanh <ndhanh96@gmail.com>
2017-12-25 21:28:49 +0700

    update div

--- jquery.js
+++ jquery.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-   $("#canvas4").hover(function() {
-      $("#canvas4").css("background", "#0bb8ff");
+   $("#box4").hover(function() {
+      $("#box4").css("background", "#0bb8ff");
    });
-   $("#canvas4").mouseleave(function(){
-      $("#canvas4").css("background", "skyblue");
+   $("#box4").mouseleave(function(){
+      $("#box4").css("background", "#094a8f");
    });
 
-   $("#canvas2").hover(function() {
-      $("#canvas2").css("background", "red");
+   $("#box2").hover(function() {
+      $("#box2").css("background", "red");
    });
-   $("#canvas2").mouseleave(function(){
-      $("#canvas2").css("background", "#bf3333");
+   $("#box2").mouseleave(function(){
+      $("#box2").css("background", "#9f0f17");
    });
 });

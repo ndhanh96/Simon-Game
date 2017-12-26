9cfa317bcbde19f18fef32817f4c9155033ebfa5
Nguyen Hanh <ndhanh96@gmail.com>
2017-12-26 16:04:48 +0700

    update text

--- jquery.js
+++ jquery.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
    $("#box4").hover(function() {
-      $("#box4").css("background", "#0bb8ff");
-   });
-   $("#box4").mouseleave(function(){
-      $("#box4").css("background", "#094a8f");
    });
 
    $("#box2").hover(function() {

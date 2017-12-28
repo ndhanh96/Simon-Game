e5b2bd849ac20d141b21e37f7f4dfc37e1e5552b
Nguyen Hanh <ndhanh96@gmail.com>
2017-12-29 02:14:44 +0700

    update all functions and finish project

--- jquery.js
+++ jquery.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+   var strictMode = false;
+   var userColor = 0;
+   var wrongColor = false;
    var times = 0;
    var count = 0;
    var startClicked = false;
@@ -6,18 +9,19 @@ $(document).ready(function() {
    var gameOn = false;
    var redLight = "red";
    var redOFF = "#993300";
-   var greenLight = "#66ff33"
+   var greenLight = "#66ff33";
    var greenOFF = "#008000";
    var blueLight = "#0099cc";
    var blueOFF = "#006699";
    var yellowLight = "#ffff66";
    var yellowOFF = "#999900";
-   var arrBot = [3, 6, 9, 12];
+   var arrBot = [];
    var arrPlayer = [];
    var redSound = "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3";
    var greenSound = "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3";
    var blueSound = "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3";
    var yellowSound = "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3";
+   var isGameRunning = true;
    // arrBot.push(random);
 
    function lightUp(box, colorON, colorOFF, sound) {
@@ -29,48 +33,71 @@ $(document).ready(function() {
       }, 1000);
    }
 
-   function TurnON() {
-      if (count > 99) {
-         reStart();
+   function Move(color) {
+      if (color == "green") {
+         lightUp("#box1", greenLight, greenOFF, greenSound);
+      } else if (color == "red") {
+         lightUp("#box2", redLight, redOFF, redSound);
+      } else if (color == "yellow") {
+         lightUp("#box3", yellowLight, yellowOFF, yellowSound);
+      } else if (color == "blue") {
+         lightUp("#box4", blueLight, blueOFF, blueSound);
       }
-      count++;
-      
-      if (count < 10) {
-         count = "0" + count.toString();
-      }
-      $("#countTime").text(count);
-      if (typeof arrBot[times] == "number") {
-         if (arrBot[times] <= 3) {
-            lightUp("#box1", greenLight, greenOFF, greenSound);
-         } else if (arrBot[times] <= 6) {
-            lightUp("#box2", redLight, redOFF, redSound);
-         } else if (arrBot[times] <= 9) {
-            lightUp("#box3", yellowLight, yellowOFF, yellowSound);
-         } else if (arrBot[times] <= 12) {
-            lightUp("#box4", blueLight, blueOFF, blueSound);
-         }
-         times++;
-      } else {
-         var random = Math.floor((Math.random() * 10) + 2);
-         if (random <= 3) {
-            lightUp("#box1", greenLight, greenOFF, greenSound);
-         } else if (random <= 6) {
-            lightUp("#box2", redLight, redOFF, redSound);
-         } else if (random <= 9) {
-            lightUp("#box3", yellowLight, yellowOFF, yellowSound);
-         } else if (random <= 12) {
-            lightUp("#box4", blueLight, blueOFF, blueSound);
-         }
-         setTimeout(function() {
-            turnOFF();
-            times = 0;
-         },1000);
-      }
-      
    }
 
-   function turnOFF(argument) {
-      // body...
+   function TurnON() {
+      isGameRunning = true;
+      userColor = 0;
+      if (count > 99) {
+         turnOFF();
+      }
+
+      if (typeof arrBot[times] == "string") {
+         Move(arrBot[times]);
+         times++;
+      } else if (typeof arrBot[times] != "string" && !wrongColor) {
+         var random = Math.floor((Math.random() * 10) + 2);
+         if (random <= 3) {
+            arrBot.push("green");
+            Move("green");
+         } else if (random <= 6) {
+            arrBot.push("red");
+            Move("red");
+         } else if (random <= 9) {
+            arrBot.push("yellow");
+            Move("yellow");
+         } else if (random <= 12) {
+            arrBot.push("blue");
+            Move("blue");
+         }
+         count++;
+         setTimeout(function() {
+            paused();
+            times = 0;
+            isGameRunning = false;
+         }, 1000);
+      } else if (typeof arrBot[times] != "string" && wrongColor) {
+         wrongColor = false;
+         setTimeout(function() {
+            paused();
+            times = 0;
+            isGameRunning = false;
+         }, 1000);
+      }
+      $("#countTime").text(count);
+
+   }
+
+   function paused() {
+      $("#box1").css("background-color", greenOFF);
+      $("#box2").css("background-color", redOFF);
+      $("#box3").css("background-color", yellowOFF);
+      $("#box4").css("background-color", blueOFF);
+      $("#countTime").text(count);
+      clearInterval(runGame);
+   }
+
+   function turnOFF() {
       $("#box1").css("background-color", greenOFF);
       $("#box2").css("background-color", redOFF);
       $("#box3").css("background-color", yellowOFF);
@@ -78,14 +105,22 @@ $(document).ready(function() {
       $("#startButton").removeClass('startButton').addClass('startButtonOff');
       clearInterval(runGame);
       $("#countTime").text("00");
+      $("#countTime").removeClass('countBoxContentON').addClass('countBoxContent');
    }
 
    $("input[type=checkbox]").click(function(event) {
       /* Act on the event */
       if (!gameOn) {
          gameOn = true;
+
       } else {
-         count = 0
+         startClicked = false
+         strictMode = false;
+         $("#strictled").removeClass('strictLedON').addClass('strictLedOFF');
+         wrongColor = false;
+         times = 0;
+         arrBot = [];
+         count = 0;
          gameOn = false;
          clearInterval(runGame);
          turnOFF();
@@ -96,12 +131,15 @@ $(document).ready(function() {
    });
 
    function reStart() {
+      // alert("restart");
       times = 0;
       count = 0;
       $("#countTime").text("00");
       $("#startButton").removeClass('startButton').addClass('startButtonOff');
+      $("#countTime").removeClass('countBoxContentON').addClass('countBoxContent');
       setTimeout(function() {
          $("#startButton").removeClass('startButtonOff').addClass('startButton');
+         $("#countTime").removeClass('countBoxContent').addClass('countBoxContentON');
       }, 500);
       clearInterval(runGame);
       turnOFF();
@@ -110,12 +148,78 @@ $(document).ready(function() {
 
    $("#startButton").click(function(event) {
       if (startClicked && gameOn) {
+         arrBot = [];
          reStart();
       } else if (!startClicked && gameOn) {
          startClicked = true;
          $("#countTime").text("00");
          $("#startButton").removeClass('startButtonOff').addClass('startButton');
+         $("#countTime").removeClass('countBoxContent').addClass('countBoxContentON');
          runGame = setInterval(function() { TurnON() }, 2000);
+      }
+   });
+
+   function playerMove(color) {
+      if (arrBot[userColor] == color) {
+         Move(color);
+         userColor++;
+         if (typeof arrBot[userColor] != "string") {
+            times = 0;
+            runGame = setInterval(function() { TurnON() }, 2000);
+         }
+      } else if (arrBot[userColor] != color && !strictMode) {
+         isGameRunning = true;
+         wrongColor = true;
+         $("#countTime").text("!!");
+         times = 0;
+         runGame = setInterval(function() { TurnON() }, 2000);
+      } else if (arrBot[userColor] != color && strictMode) {
+         isGameRunning = true;
+         $("#countTime").text("!!");
+         setTimeout(function() {
+            arrBot = [];
+            userColor = 0;
+            wrongColor = false;
+            reStart();
+         }, 1000);
+      }
+   }
+
+   $("#strictButton").click(function() {
+      if (!startClicked && gameOn && !strictMode) {
+         strictMode = true;
+         $("#strictled").removeClass('strictLedOFF').addClass('strictLedON');
+      } else if (strictMode && gameOn && startClicked) {
+         strictMode = false;
+         $("#strictled").removeClass('strictLedON').addClass('strictLedOFF');
+         arrBot = [];
+         userColor = 0;
+         wrongColor = false;
+         reStart();
+      } else if (strictMode && gameOn && !startClicked) {
+         strictMode = false;
+         $("#strictled").removeClass('strictLedON').addClass('strictLedOFF');
+      }
+   });
+
+   $("#box1").click(function() {
+      if (!isGameRunning) {
+         playerMove("green");
+      }
+   });
+   $("#box2").click(function() {
+      if (!isGameRunning) {
+         playerMove("red");
+      }
+   });
+   $("#box3").click(function() {
+      if (!isGameRunning) {
+         playerMove("yellow");
+      }
+   });
+   $("#box4").click(function() {
+      if (!isGameRunning) {
+         playerMove("blue");
       }
    });
 });
